eede8156d996663fa4b6154d677f4833680cafee
Raphael Frogoso <alkali.smiley@gmail.com>
2024-03-07 13:35:35 +0000

    replaced all images in grid gallery in gallery page with vnl images

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -5,7 +5,7 @@
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
                 <h1 class="home-h1">
-                    Do you want to become a volleyball player?
+                    Featured: Japan Team
                 </h1>
                 <a 
                     href="/blog"
@@ -21,16 +21,16 @@
     <!-- Gallery -->
     <div class="gallery-row">
         <div class="gallery-col">
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" class="gallery-img" alt="Boat on Calm Water" />
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp" class="gallery-img" alt="Wintry Mountain Landscape" />
+            <img src="https://i.pinimg.com/736x/66/2b/2b/662b2bd2ce5d11dc6862b544ee3a6549.jpg" class="gallery-img" alt="Boat on Calm Water" />
+            <img src="https://i.pinimg.com/564x/7d/a8/ae/7da8aed96cb240cdcf982a65c64acc4a.jpg" class="gallery-img" alt="Wintry Mountain Landscape" />
         </div>
         <div class="gallery-col">
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp" class="gallery-img" alt="Mountains in the Clouds" />
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" class="gallery-img" alt="Boat on Calm Water" />
+            <img src="https://i.pinimg.com/564x/2e/ea/58/2eea580371c6ea2ae7483d9c82781f19.jpg" class="gallery-img" alt="Mountains in the Clouds" />
+            <img src="https://i.pinimg.com/564x/f9/87/18/f98718e914cfa9cbf4e3ebb9ea776613.jpg" class="gallery-img" alt="Boat on Calm Water" />
         </div>
         <div class="gallery-col">
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp" class="gallery-img" alt="Waves at Sea" />
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp" class="gallery-img" alt="Yosemite National Park" />
+            <img src="https://i.pinimg.com/564x/ed/4e/53/ed4e53a1d687659dd5e71a65d63cce4a.jpg" class="gallery-img" alt="Waves at Sea" />
+            <img src="https://i.pinimg.com/564x/d6/b7/82/d6b782546cfcbc99d2c4763a114bb4bf.jpg" class="gallery-img" alt="Yosemite National Park" />
         </div>
     </div>
 @endsection
