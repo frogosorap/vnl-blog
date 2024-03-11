f1058de747d1be9bf9b08081f7ce2a4a9108783c
Raphael Frogoso <alkali.smiley@gmail.com>
2024-03-11 18:53:41 +0000

    Few adjustments and added more div blocks for the image gallery page

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -25,43 +25,92 @@
     <div class="grid-container">
         <div>
           <img class='grid-item grid-item-1' src='https://i.pinimg.com/736x/66/2b/2b/662b2bd2ce5d11dc6862b544ee3a6549.jpg' alt=''>
-          <p>"I'm so happy today!"</p>
+          <p>"Sekita on point today!"</p>
         </div>
         <div>
-          <img class='grid-item grid-item-2' src='https://i.pinimg.com/564x/7d/a8/ae/7da8aed96cb240cdcf982a65c64acc4a.jpg' alt=''>
-          <p>"I see those nugs."</p>
+          <img class='grid-item grid-item-2' src='https://i.pinimg.com/564x/43/db/b5/43dbb5d111e37d66bce4b7d53bf20064.jpg' alt=''>
+          <p>"We got that cheeky point :P"</p>
         </div>
         <div>
-          <img class='grid-item grid-item-3' src='https://i.pinimg.com/564x/80/02/48/800248bafc52507394f77107ae294a9e.jpg' alt=''>
-          <p>"I love you so much!"</p>
-        </div>
+            <img class='grid-item grid-item-4' src='https://i.pinimg.com/564x/0a/8f/ef/0a8fef1c4576f7002f19721ff1af87da.jpg' alt=''>
+            <p>"Kai got subbed whut :O"</p>
+          </div>
         <div>
-          <img class='grid-item grid-item-4' src='https://i.pinimg.com/564x/0a/8f/ef/0a8fef1c4576f7002f19721ff1af87da.jpg' alt=''>
-          <p>"I'm the baby of the house!"</p>
+          <img class='grid-item grid-item-3' src='https://i.pinimg.com/564x/3f/69/6f/3f696f787157d538ac635d41b62da921.jpg' alt=''>
+          <p>"JAPAN IN PHILIPPINES! 🇯🇵"</p>
         </div>
         <div>
           <img class='grid-item grid-item-5' src='https://i.pinimg.com/564x/57/04/93/570493b6c6b1ff47c81b5f0afe7d3f6d.jpg' alt=''>
-          <p>"Are you gunna throw the ball?"</p>
+          <p>"Are you GUYS ready!?"</p>
         </div>
         <div>
           <img class='grid-item grid-item-6' src='https://i.pinimg.com/564x/d6/e0/6d/d6e06d807b8857c2ba4ccadb2f229fbf.jpg' alt=''>
-          <p>"C'mon friend!"</p>
+          <p>"Kai with the ace???"</p>
         </div>
         <div>
           <img class='grid-item grid-item-7' src='https://i.pinimg.com/564x/c3/5d/83/c35d833e57e7486f851f391b30d050b9.jpg' alt=''>
-          <p>"A rose for mommy!"</p>
+          <p>"Too fun to warmup."</p>
         </div>
         <div>
           <img class='grid-item grid-item-8' src='https://i.pinimg.com/564x/0a/97/51/0a97512030080bce47e096613f0d6931.jpg' alt=''>
-          <p>"You gunna finish that?"</p>
+          <p>"The Three Musketeers!"</p>
         </div>
         <div>
           <img class='grid-item grid-item-9' src='https://i.pinimg.com/564x/1d/cf/64/1dcf640eefa810491ed3d0b61abc693e.jpg' alt=''>
-          <p>"We can't afford a cat!"</p>
+          <p>"Japan for BRONZE!!!"</p>
         </div>
         <div>
           <img class='grid-item grid-item-10' src='https://i.pinimg.com/564x/60/9d/15/609d158d913be6b7c2ed9a4ccd76eb72.jpg' alt=''>
-          <p>"Dis my fren!"</p>
+          <p>"Timeout's getting out of hand."</p>
         </div>
+        <div>
+            <img class='grid-item grid-item-10' src='https://i.pinimg.com/564x/08/b7/ff/08b7ffc40d37788cc4d74c05e6a929f9.jpg' alt=''>
+            <p>"Is this time for Yuki's return?"</p>
+        </div>
+        <div>
+            <img class='grid-item grid-item-10' src='https://i.pinimg.com/564x/85/d6/18/85d618d9eea7e1166e3edea6cf0e95cd.jpg' alt=''>
+            <p>"It's about time Japan shows up."</p>
+        </div>
+        <div>
+            <img class='grid-item grid-item-10' src='https://i.pinimg.com/564x/a4/85/7b/a4857b4e8d8958fa953ce82aaf2f6c06.jpg' alt=''>
+            <p>"Monster YUJI NISHIDA!"</p>
+        </div>
+        <div>
+            <img class='grid-item grid-item-10' src='https://i.pinimg.com/564x/13/fd/24/13fd24c89cf1aa136aef8fa13b0c1cbf.jpg' alt=''>
+            <p>"What's going on here? It's 2023."</p>
+        </div>
+        <div>
+            <img class='grid-item grid-item-10' src='https://i.pinimg.com/564x/d0/93/a5/d093a561e91f67bfb1c10e3fd23a1997.jpg' alt=''>
+            <p>"Too much chatting going on..."</p>
+        </div>
+        <div>
+            <img class='grid-item grid-item-10' src='https://i.pinimg.com/564x/cc/7b/52/cc7b52281e01d2cec4473a1065f6b53e.jpg' alt=''>
+            <p>"Thank you Philippines 2023!"</p>
+        </div>
+        <div>
+            <img class='grid-item grid-item-10' src='https://i.pinimg.com/564x/89/14/ab/8914ab7025abc67b80cc9acf1ab25579.jpg' alt=''>
+            <p>"Challenge Incoming *Gulp*"</p>
+        </div>
+        <div>
+            <img class='grid-item grid-item-10' src='https://i.pinimg.com/564x/fb/e4/1e/fbe41ef7e786dcdb33dde302bea1cbc9.jpg' alt=''>
+            <p>"The power DUO OMG!"</p>
+        </div>
+        <div>
+            <img class='grid-item grid-item-10' src='https://i.pinimg.com/564x/83/a7/4a/83a74add1ccb2f8541029004ea6640ac.jpg' alt=''>
+            <p>"Did you say giveaways??"</p>
+        </div>
+        <div>
+            <img class='grid-item grid-item-10' src='https://i.pinimg.com/564x/e5/72/73/e57273df24c69fba3d7f786e25ba7eb8.jpg' alt=''>
+            <p>"Challenge Incoming V2 *Gulp*"</p>
+        </div>
+        <div>
+            <img class='grid-item grid-item-10' src='https://i.pinimg.com/564x/94/8d/8c/948d8cba0a3e7b60f1228e9f5f2bbc14.jpg' alt=''>
+            <p>"WE'RE THROUGH THE FINALS!"</p>
+        </div>
+        <div>
+            <img class='grid-item grid-item-10' src='https://i.pinimg.com/564x/7e/6a/87/7e6a87bff1cf653ac0d6125d99d75acf.jpg' alt=''>
+            <p>"GET BLOCKED BRAZIL!"</p>
+        </div>
+
       </div>
 @endsection
