8198f77cc6fe206506608003ff1431d2649844c0
Raphael Frogoso <alkali.smiley@gmail.com>
2024-03-12 10:24:52 +0000

    navbar changes with css and positioning of links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 </head>
 <body class="bg-gray-100 h-screen antialiased leading-none font-sans">
     <div id="app">
-        <header class="bg-blue-900 py-6">
+        <header id="navbar"class="navbar">
             <div class="container mx-auto flex justify-between items-center px-6">
                 <div>
                     <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
