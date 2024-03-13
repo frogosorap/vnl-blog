78c56a1bb8b71fe738236e623387819f64833195
Raphael Frogoso <alkali.smiley@gmail.com>
2024-03-13 11:57:29 +0000

    Making navbar sticky to the top of pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,20 @@
                 </nav>
             </div>
         </header>
+        <script>
+            window.onscroll = function() {stickyNavbar()};
+    
+            var navbar = document.querySelector('.navbar');
+            var sticky = navbar.offsetTop;
+    
+            function stickyNavbar() {
+            if (window.pageYOffset >= sticky) {
+                navbar.classList.add("sticky");
+            } else {
+                navbar.classList.remove("sticky");
+            }
+            }
+        </script>
 
         @yield('content')
     </div>
